aaf2491deb0e00bade9efe356934c18868fba404
Toni Menzel <toni.menzel@rebaze.com>
2019-07-11 13:54:02 +0200

    allow full cycle test using a single  command

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+##############################################################################
+##
+##  Full Cycle Test Script
+##
+##############################################################################
+
+./gradlew -b ../febo/build.gradle publishToMavenLocal --P baseline=true && ./gradlew test -i
