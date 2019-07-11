fd1e1be5c8b24641f899d9ffe2ede6a44634a75a
Toni Menzel <toni.menzel@rebaze.com>
2019-07-11 13:54:18 +0200

    use new boot abstraction

--- application-deployment/src/main/java/org/tablerocket/example/App.java
+++ application-deployment/src/main/java/org/tablerocket/example/App.java
@@ -2,9 +2,9 @@ package org.tablerocket.example;
 
 import org.tablerocket.example.calculator.CalculatorBundle;
 import org.tablerocket.example.service.BookingService;
+import org.tablerocket.febo.api.Boot;
 import org.tablerocket.febo.api.Febo;
 import org.tablerocket.febo.autobundle.AutoBundleSupport;
-import org.tablerocket.febo.core.OSGiFebo;
 import org.tablerocket.febo.repository.ClasspathRepositoryStore;
 
 /**
@@ -14,13 +14,13 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         // Optional concept from febo-autobundle allowing bundleization of parts of the classpath from here based on convention.
-
         AutoBundleSupport autoBundle = new AutoBundleSupport();
-        Febo app = OSGiFebo.febo()
+        Febo app = Boot.febo()
                 .platform(new ClasspathRepositoryStore())
                 .require(autoBundle.from(BookingService.class))
                 .require(autoBundle.from(CalculatorBundle.class).withAutoExportApi(true))
-                .keepRunning(true);
+                //.keepRunning(true)
+        ;
         app.run(new String[] {"21","99"});
     }
 }
--- application-deployment/src/test/java/org/tablerocket/example/calculator/CalcTest.java
+++ application-deployment/src/test/java/org/tablerocket/example/calculator/CalcTest.java
@@ -2,15 +2,15 @@ package org.tablerocket.example.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.tablerocket.example.mytest.MyTestEntry;
+import org.tablerocket.febo.api.Boot;
 import org.tablerocket.febo.autobundle.AutoBundleSupport;
-import org.tablerocket.febo.core.OSGiFebo;
 import org.tablerocket.febo.repository.ClasspathRepositoryStore;
 
 class CalcTest {
     @Test
     void simpleTest() throws Exception {
         AutoBundleSupport autoBundle = new AutoBundleSupport();
-        OSGiFebo.febo()
+        Boot.febo()
                 .platform(new ClasspathRepositoryStore())
                 .require(autoBundle.from(MyTestEntry.class))
                 .require(autoBundle.from(CalculatorBundle.class).withAutoExportApi(true))
